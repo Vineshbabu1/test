f2e76461c0d6242583c317ee6622ccf2cce988c0
Sriram K <sriram.k@payoda.com>
2021-07-21 19:25:18 +0530

    Changes made to the Java files
    * Added Ball Java code.
    * Added print line to Apple Java code.

--- Ball.java
+++ Ball.java
@@ -1,10 +1,11 @@
-public class Apple {
+public class Ball {
     public static void print_line() {
         System.out.println("Some line to print...");
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Ball World!");
+
         print_line();
     }
 }
